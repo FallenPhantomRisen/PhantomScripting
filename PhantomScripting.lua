5f0415600bfc390bba19b1d35fac72753f712851
FallenPhantomRisen <77451316+FallenPhantomRisen@users.noreply.github.com>
2022-10-27 13:48:03 -0400

    Update PhantomScripting.lua

--- PhantomScripting.lua
+++ PhantomScripting.lua
@@ -1,4 +1,3 @@
-getgenv().SecureMode = true
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 local Window = Rayfield:CreateWindow({
 Name = "Phantom Scripting",
